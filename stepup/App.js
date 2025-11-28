06343ae7690d285d89d93be0fd4e95314bc0529b
Yashsingh045 <astomar6396@gmail.com>
2025-11-28 23:02:13 +0530

    feat: Add Default landing screen

--- stepup/App.js
+++ stepup/App.js
@@ -1,19 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Dashboard from './src/screens/dashboard';
+import AppNavigation from './src/navigation/navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Dashboard />
+    <>
+      <AppNavigation />
       <StatusBar style="light" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-});
